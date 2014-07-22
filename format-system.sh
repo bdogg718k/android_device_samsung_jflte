30b29702ccea00062f5c49f30b664f7f78296877
CPA-Poke <brock.shahan@cox.net>
2014-07-22 12:00:00 -0500

    Remove unnecessary fs_type checks
    Change-Id: I2928cead7a339c5065397274dc9f8e6bb2036cde

--- format-system.sh
+++ format-system.sh
@@ -1,17 +1,11 @@
 #!/sbin/sh
 
-# find out which partitions are formatted as F2FS
-mount | grep -q 'data type f2fs'
-DATA_F2FS=$?
-mount | grep -q 'cache type f2fs'
-CACHE_F2FS=$?
+# find out the fs_type of /system so it is formatted correctly
 mount | grep -q 'system type f2fs'
 SYSTEM_F2FS=$?
 
-if [ $DATA_F2FS -eq 0 ] && [ $CACHE_F2FS -eq 0 ] && [ $SYSTEM_F2FS -eq 0 ]; then
+if [ $SYSTEM_F2FS -eq 0 ]; then
   mkfs.f2fs /dev/block/platform/msm_sdcc.1/by-name/system
-elif [ $DATA_F2FS -eq 0 ] && [ $CACHE_F2FS != 0 ] && [ $SYSTEM_F2FS != 0 ]; then
+elif [ $SYSTEM_F2FS != 0 ]; then
    mkfs.ext4 /dev/block/platform/msm_sdcc.1/by-name/system
-elif [ $DATA_F2FS != 0 ] && [ $CACHE_F2FS != 0 ] && [ $SYSTEM_F2FS != 0 ]; then
-    mkfs.ext4 /dev/block/platform/msm_sdcc.1/by-name/system
 fi
