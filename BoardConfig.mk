0526f322f6bc43b7a835b37c3ebefd708c0107ac
CPA-Poke <brock.shahan@cox.net>
2014-08-17 08:33:23 -0500

    Move back to PoKernel after rebase
    Change-Id: Idebdb65aa240e83de2e7a2784dd3e59e46a835d3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.f
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := cyanogen_jf_defconfig
+TARGET_KERNEL_CONFIG := poke_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/jf
 # TARGET_GCC_VERSION_ARM := 4.10-sm
 # TARGET_GCC_VERSION_AND := 4.8-sm
